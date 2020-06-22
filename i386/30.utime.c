88f358248329a5beb9f9c7f2b814a7ed5548d091
aizsfgk <1732756017@qq.com>
2020-06-22 16:27:54 +0800

    add utime|access

--- i386/30.utime.c
+++ i386/30.utime.c
@@ -1,10 +1,65 @@
 #include <sys/types.h>
 #include <utime.h>
 
+#include <sys/stat.h> // stat函数
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <time.h>
+#include <sys/time.h>
+
 
 // utime, utimes - change file last access and modification times
 int main(int argc, char const *argv[])
 {
+	char buf[100];
 	
+	struct stat st;
+	const char * file_name = "./file/utime.file.txt";
+	int ret = stat(file_name, &st);
+	if (ret == 0) {
+		fprintf(stdout, "st_atim => tv_sec: %ld, tv_nsec: %ld\n", (long) st.st_atim.tv_sec, (long)  st.st_atim.tv_nsec);
+		fprintf(stdout, "st_mtim => tv_sec: %ld, tv_nsec: %ld\n", (long) st.st_mtim.tv_sec, (long)  st.st_mtim.tv_nsec);
+
+		struct tm *tm_local;
+		tm_local = localtime(&st.st_atim.tv_sec);
+		strftime(buf, 100, "[%Y-%m-%d %H:%M:%S]", tm_local);
+		fprintf(stdout, "访问时间: %s\n", buf);
+
+
+		tm_local = localtime(&st.st_mtim.tv_sec);
+		strftime(buf, 100, "[%Y-%m-%d %H:%M:%S]", tm_local);
+		fprintf(stdout, "修改时间: %s\n", buf);
+
+		// 修改时间
+		
+		struct utimbuf u_time_buf;
+		u_time_buf.actime =  time(NULL) + 10;
+		u_time_buf.modtime = time(NULL) + 100;
+		ret = utime(file_name, &u_time_buf);
+		if (ret == 0) {
+			fprintf(stdout, "%s\n", "utime_ok");
+
+			tm_local = localtime(&st.st_atim.tv_sec);
+			strftime(buf, 100, "[%Y-%m-%d %H:%M:%S]", tm_local);
+			fprintf(stdout, "访问时间: %s\n", buf);
+
+
+			tm_local = localtime(&st.st_mtim.tv_sec);
+			strftime(buf, 100, "[%Y-%m-%d %H:%M:%S]", tm_local);
+			fprintf(stdout, "修改时间: %s\n", buf);
+
+
+		} else {
+			perror("utime");
+			exit(EXIT_FAILURE);
+		}
+
+	} else {
+		perror("stat");
+		exit(EXIT_FAILURE);
+	}
 	return 0;
 }
--- i386/33.access.c
+++ i386/33.access.c
@@ -1,10 +1,25 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+// access - check real user’s permissions for a file
 
 int main(int argc, char const *argv[])
 {
-	
+	const char * fileName = "file/access.file.txt";
+	int ret = access(fileName, F_OK);
+	if (ret == -1) {
+		fprintf(stderr, "%s : %s\n", fileName, "file not exists");
+	}
 
-	
+	const char *fileName2 = "file/utime.file.txt";
+	ret = access(fileName2, F_OK|R_OK|W_OK);
+	if (ret == 0) {
+		fprintf(stdout, "%s\n", "can rw");
+	} else {
+		perror("access");
+		exit(EXIT_FAILURE);
+	}
+
 	return 0;
 }
