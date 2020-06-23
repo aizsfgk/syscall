b9876584c1dd0c84f330dbc4b6f63c0df9435e81
aizsfgk <1732756017@qq.com>
2020-06-23 09:48:24 +0800

    add dup

--- i386/41.dup.c
+++ i386/41.dup.c
@@ -3,6 +3,14 @@
 
 int main(int argc, char const *argv[])
 {
-	
+
+	int new_stdout = dup2(1, 10);
+	if (new_stdout > 0) {
+		write(new_stdout, "yes\n", 4);
+		fprintf(stdout, "new_stdout: %d\n", new_stdout);
+	} else {
+		perror("dup2");
+		exit(EXIT_FAILURE);
+	}
 	return 0;
 }
---
+++ i386/42.pipe.c
@@ -0,0 +1,9 @@
+#include "base.h"
+
+// pipe, pipe2 - create pipe
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
