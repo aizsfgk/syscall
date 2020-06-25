4bcf8ade5cfea49c3a2e6b3e6cd3bbcb223a9c67
aizsfgk <1732756017@qq.com>
2020-06-25 09:31:38 +0800

    add setpgid

---
+++ i386/55.fcntl.c
@@ -0,0 +1,40 @@
+#include "base.h"
+#include <fcntl.h>
+
+// fcntl - manipulate file descriptor
+// 
+//  #include <unistd.h>
+//  #include <fcntl.h>
+//
+//  int fcntl(int fd, int cmd, ... /* arg */ );
+//  
+//  1. 复制文件描述符
+//  
+//  
+//  2. 文件描述符标志位
+//  
+//  
+//  3. 文件状态标志位
+//  
+//  
+//  4. Advisory locking 乐观锁
+//  
+//  
+//  5. Mandatory locking 悲观锁，强制锁
+//  
+//  
+//  6. 管理信号
+//  
+//  
+//  7. 租约
+//  
+//  
+//  8. 文件和目录改变通知
+//  
+//  
+//  
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
---
+++ i386/57.setpgid.c
@@ -0,0 +1,25 @@
+#include "base.h"
+
+// setpgid, getpgid, setpgrp, getpgrp - set/get process group
+
+
+int main(int argc, char const *argv[])
+{
+	int  rt = setpgid(getpid(), 0);
+	if (rt == 0) {
+		fprintf(stdout, "%s\n", "ok");
+		pid_t pid = getpid();
+		pid_t gpid = getpgrp();
+
+		fprintf(stdout, "cur process gpid: %d, pid: %d\n", 
+			(int)gpid, (int)pid);
+	} else {
+		perror("setpgid");
+		// exit(EXIT_FAILURE);
+	}
+
+
+	pid_t gpid = getpgid(getpid());
+	fprintf(stdout, "cur process gpid: %d\n", (int)gpid);
+	return 0;
+}
