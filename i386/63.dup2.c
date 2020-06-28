d3d7a10203aea12ce6631c15fe1467a1a1c45b13
aizsfgk <1732756017@qq.com>
2020-06-28 17:31:19 +0800

    add dup2

--- i386/63.dup2.c
+++ i386/63.dup2.c
@@ -2,6 +2,15 @@
 
 int main(int argc, char const *argv[])
 {
-	
+	int new_fd = dup2(1, 123);
+	if (new_fd > 0) {
+		write(123, "hello-dup2\n", sizeof("hello-dup2"));
+	} else {
+		perror("dup2");
+		exit(EXIT_FAILURE);
+	}
+
+
+
 	return 0;
 }
