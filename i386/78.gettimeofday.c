906b316f0cb3606b16da1799d4fe566159c8839e
aizsfgk <1732756017@qq.com>
2020-07-03 20:05:30 +0800

    add gettimeofday | getgroups

--- i386/78.gettimeofday.c
+++ i386/78.gettimeofday.c
@@ -1,7 +1,21 @@
 #include "base.h"
 
+#include <sys/time.h>
+
 int main(int argc, char const *argv[])
 {
+	struct timeval tv;
+
+	int rt = gettimeofday(&tv, NULL);
+	if (rt == -1) {
+		perror("gettimeofday");
+		exit(EXIT_FAILURE);
+	}
+
+
+	fprintf(stdout, "sec: %ld\n", tv.tv_sec);
+	fprintf(stdout, "usec: %ld\n", tv.tv_usec);
+
 	
 	return 0;
 }
--- i386/80.getgroups.c
+++ i386/80.getgroups.c
@@ -2,6 +2,16 @@
 
 int main(int argc, char const *argv[])
 {
-	
+	gid_t list[10];
+	int i;
+	int rt = getgroups(10, list);
+	if (rt == 0) {
+		for (i=0; i<10; i++) {
+			printf(" %d ==> %d\n", i, list[i]);
+		}
+	} else {
+		perror("getgroups");
+		exit(EXIT_FAILURE);
+	}
 	return 0;
 }
