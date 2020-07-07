faf5b55fc6a9fe09a04b730dffc73b8a3ce030fd
aizsfgk <1732756017@qq.com>
2020-07-07 21:39:36 +0800

    add readlink

--- i386/85.readlink.c
+++ i386/85.readlink.c
@@ -1,7 +1,20 @@
 #include "base.h"
 
+// 读取link文件
+
 int main(int argc, char const *argv[])
 {
+
+	char buf[200];
+
+	// buf 存储真实的文件名
 	
+	ssize_t rd_size = readlink("./file/link_path_file.txt", buf, 200);
+	if (rd_size > 0) {
+		fprintf(stdout, "%s\n", buf);
+	} else {
+		perror("readlink");
+		exit(EXIT_FAILURE);
+	}
 	return 0;
 }
