994aeef2f6f9f8c9eae53bccc3a49efcb70ce71c
aizsfgk <1732756017@qq.com>
2020-07-07 12:24:30 +0800

    add symlink

--- i386/83.symlink.c
+++ i386/83.symlink.c
@@ -1,7 +1,27 @@
 #include "base.h"
 
+//  symlink, symlinkat - make a new name for a file
+
+
 int main(int argc, char const *argv[])
 {
-	
+	const char *target = "file/link_target.txt";
+	const char *linkpath = "./file/link_path_file.txt";
+
+
+	char cwd[100];
+	char *pcwd = realpath(target, cwd);
+	if (pcwd == NULL) {
+		perror("realpath");
+		exit(EXIT_FAILURE);
+	}
+
+	int rt = symlink(cwd, linkpath);
+	if (rt == 0) {
+		fprintf(stdout, "ok\n");
+	} else {
+		perror("symlink");
+		exit(EXIT_FAILURE);
+	}
 	return 0;
 }
