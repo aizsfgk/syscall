50dd3af216b24ee787ccc4025c440416a11830d2
aizsfgk <1732756017@qq.com>
2020-07-20 17:41:09 +0800

    add statfs

--- i386/99.statfs.c
+++ i386/99.statfs.c
@@ -1,1 +1,22 @@
+#include "base.h"
+#include <sys/vfs.h>
+
+
 // statfs, fstatfs - get filesystem statistics
+// 
+// 获取文件系统统计信息
+// 
+
+int main(int argc, char const *argv[])
+{
+	const char * file = "./file/file.txt";
+	struct statfs sfs;
+	int rs = statfs(file, &sfs);
+	if (rs == -1) {
+		perror("statfs");
+		exit(EXIT_FAILURE);
+	}
+
+	fprintf(stdout, "f_type: %x\n", (int) sfs.f_type); // EXT4_SUPER_MAGIC      0xEF53
+	return 0;
+}
