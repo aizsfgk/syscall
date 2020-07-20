b4392fbd548507a27bb09b07e426333c7c584731
aizsfgk <1732756017@qq.com>
2020-07-20 17:31:58 +0800

    add setpriority/getpriority

--- i386/96.getpriority.c
+++ i386/96.getpriority.c
@@ -1,9 +1,21 @@
 #include "base.h"
+#include <sys/time.h>
+#include <sys/resource.h>
 
 // 获取进程优先级
 
 int main(int argc, char const *argv[])
 {
 	
+	int rs = setpriority(PRIO_PROCESS, getpid(), 10);
+	if (rs  == -1) {
+		perror("setpriority");
+		exit(EXIT_FAILURE);
+	}
+
+
+	rs = getpriority(PRIO_PROCESS, getpid());
+	fprintf(stdout, "rs: %d\n", rs);
+
 	return 0;
 }
